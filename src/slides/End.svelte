ac014694c815f8e11bd725538bd90ac7b8f30c33
Rob Bateman <rob@infiniteturtles.co.uk>
2023-11-15 04:21:38 +0000

    adjust coolmath logo size

--- src/slides/End.svelte
+++ src/slides/End.svelte
@@ -2,7 +2,7 @@
     <img style="margin: 0px;" src="assets/thank_you.png" alt="An Emulator's Journey" height="400" width="1200">
     <div class="r-hstack">
         <img style="margin: 0px 100px 0px 100px;" src="assets/logo-poki.png" alt="Poki" width="200" height="200">
-        <img style="margin: 0px 100px 0px 100px;" src="assets/logo-coolmathgames.png" alt="Coolmath Games" width="200" height="200">
+        <img style="margin: 0px 100px 0px 100px;" src="assets/logo-coolmathgames.png" alt="Coolmath Games" width="300" height="300">
     </div>
     <div class="r-stretch">
     </div>
